64fe98fdc617880454d7236ef7262803dc507d0e
임현준 <baj2121@naver.com>
2020-09-09 16:37:48 +0900

    https port 등록

--- sanpomichi/src/main/java/com/sanpo/run/SanpomichiApplication.java
+++ sanpomichi/src/main/java/com/sanpo/run/SanpomichiApplication.java
@@ -1,9 +1,13 @@
 package com.sanpo.run;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -20,5 +24,17 @@ public class SanpomichiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SanpomichiApplication.class, args);
 	}
+	@Bean
+    public ServletWebServerFactory serveltContainer(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8060);
+        return connector;
+    }
 
 }
